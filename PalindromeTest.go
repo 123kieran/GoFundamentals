6564bc1ec3b713312b6a5248089dda9f5215b4c3
123kieran <123kieran@gmail.com>
2017-10-06 12:19:00 +0100

    finished

--- PalindromeTest.go
+++ PalindromeTest.go
@@ -1,4 +1,4 @@
-//Kieran O'Halloran
+//Kieran O'Halloran    fff
 
 package main
 
@@ -7,8 +7,8 @@ import "math/rand"
 import "time"
 
 func random(min, max int) int {
-    rand.Seed(time.Now().Unix())
-    return rand.Intn(max - min) + min
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
 }
 
 func main() {
@@ -17,20 +17,18 @@ func main() {
 	var count int = 0
 	var exit bool = true
 	randomNumber := random(1, 100)
-	var numbersChosen[100] int
-	
+	var numbersChosen [100]int
 
-	for exit{
+	for exit {
 
-		fmt.Print("Please enter a number(1-100):\n" )
-		fmt.Scan(&userGuess) 
+		fmt.Print("Please enter a number(1-100):\n")
+		fmt.Scan(&userGuess)
 
-		
 		count++
 
-		for i := 0; i < 100; i++{
-			if userGuess == numbersChosen[i]{
-				fmt.Print("You already guessed that number\n" )
+		for i := 0; i < 100; i++ {
+			if userGuess == numbersChosen[i] {
+				fmt.Print("You already guessed that number\n")
 				count--
 			}
 		}
@@ -38,14 +36,13 @@ func main() {
 		numbersChosen[count] = userGuess
 
 		if randomNumber == userGuess {
-			fmt.Printf("You guessed the number! It took you %d goes\n", count )
+			fmt.Printf("You guessed the number! It took you %d goes\n", count)
 			exit = false
-		}else if randomNumber > userGuess {
-			fmt.Print("Your guess was too low, try again!\n" )
-		}else if randomNumber < userGuess {
-			fmt.Print("Your guess was too high, try again!\n" )
+		} else if randomNumber > userGuess {
+			fmt.Print("Your guess was too low, try again!\n")
+		} else if randomNumber < userGuess {
+			fmt.Print("Your guess was too high, try again!\n")
 		}
 	}
-
 
 }
